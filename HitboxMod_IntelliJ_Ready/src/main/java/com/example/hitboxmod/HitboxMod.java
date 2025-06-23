8404b1cabc110b0cfa944ad322e5f45243748c3d
fafadafa1 <matvejk820@gmail.com>
2025-06-23 13:38:21 +0300

    Add files via upload

---
+++ HitboxMod_IntelliJ_Ready/src/main/java/com/example/hitboxmod/HitboxMod.java
@@ -0,0 +1,10 @@
+package com.example.hitboxmod;
+
+import net.fabricmc.api.ModInitializer;
+
+public class HitboxMod implements ModInitializer {
+    @Override
+    public void onInitialize() {
+        System.out.println("Hitbox Mod initialized!");
+    }
+}
